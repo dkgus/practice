e652a7023ee33efb0fb9e38f6813867ebe0759fc
220dev <220.devv@gmail.com>
2021-06-15 16:15:46 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,54 @@
+//설치한 모듈 불러오기
+const express = require('express')
+const socket = require('socket.io')
+
+//Node.js 기본 내장 모듈 불러오기
+const http = require('http')
+const fs = require('fs')
+
+//express 객체 생성
+const app = express()
+
+//express http 서버 생성
+const server = http.createServer(app)
+
+//생성된 서버를 socket.io에 바인딩
+const io = socket(server)
+
+app.use('/css', express.static('./static/css'))
+app.use('/js', express.static('./static/js'))
+
+app.get('/', function(request, response) {
+  fs.readFile('./static/index.html', function(err, data) {
+    if(err) {
+      response.send('에러')
+    } else {
+      response.writeHead(200, {'Content-Type':'text/html'})
+      response.write(data)
+      response.end()
+    }
+  })
+})
+
+io.sockets.on('connection', function(socket) {
+  socket.on('newUser', function(name) {
+    console.log(name + ' 님이 접속하였습니다.')
+    socket.name = name
+    io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
+  })
+
+  socket.on('message', function(data) {
+    data.name = socket.name
+    console.log(data)
+    socket.broadcast.emit('update', data);
+  })
+
+  socket.on('disconnect', function() {
+    console.log(socket.name + '님이 나가셨습니다.')
+    socket.broadcast.emit('update', {type: 'disconnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'});
+  })
+})
+
+server.listen(3000, function() {
+  console.log('서버 실행 중..')
+})
